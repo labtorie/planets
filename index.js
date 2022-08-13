4f7b410ae0e3f657f17fb1dc1a6756f1eebe343c
labtorie <labtorie@gmail.com>
2022-08-13 23:19:49 +0300

    scale

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
-canvas.height = window.innerHeight - 60
+canvas.height = window.innerHeight
 
 const FRAMERATE = 90
 const TIMEOUT = 1000 / FRAMERATE
-const PLANET_SCALE = .5
 
 const G = 1
 
 document.querySelector('#trace').addEventListener('change', function () {
+
     config.renderTrace = this.checked
 })
+
 document.querySelector('#forces').addEventListener('change', function () {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
     config.renderForces = this.checked
 })
+/*document.querySelector('#follow').addEventListener('change', function () {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    config.followCam = this.checked
+})*/
 
+document.querySelector('#scale').addEventListener('change', function () {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    config.scale = this.value
+})
 
 
 function force (p1, p2) {
@@ -28,32 +38,36 @@ function force (p1, p2) {
 config = {
     renderTrace: false,
     renderForces: false,
+    followCam: false,
+    scale: .5
 }
+
 
 const planets = [
     {
         color: 'blue',
         mass: 1500,
-        coords: new Vector2(canvas.width / 4, canvas.height / 2),
+        coords: new Vector2(-400, 0),
         velocity: new Vector2(0, 1.6)
     },
     {
         color: 'gray',
         mass: 500,
-        coords: new Vector2(canvas.width / 2.4, canvas.height / 2),
+        coords: new Vector2(120, 0),
         velocity: new Vector2(0, 3)
     },
     {
         color: 'white',
         mass: 20,
-        coords: new Vector2(canvas.width / 4 + 40 , canvas.height / 2),
+        coords: new Vector2(-360, 0),
         velocity: new Vector2(0, 2.3)
     },
     {
-        color: 'red',
+        color: '#ffd6d6',
+        glow: true,
         mass: 100000,
-        coords: new Vector2(canvas.width / 2, canvas.height / 2),
-        velocity: new Vector2(0, -.03)
+        coords: new Vector2(0,0),
+        velocity: new Vector2(0, -.05)
     },
 
 ]
@@ -125,14 +139,16 @@ function drawCircle(ctx, x, y, radius, fill) {
     }
 }
 
+
 class Planet {
-    constructor(color, mass, coords, velocity) {
+    constructor({color, mass, coords, velocity, glow}) {
+        this.glow = !!glow
         this.id = Math.floor(Math.random()*10000).toString()
         this.color = color
         this.mass = mass
         this.coords = coords
         this.velocity = velocity
-        this.radius = PLANET_SCALE * Math.cbrt((3 * this.mass) / 4 * Math.PI)
+        this.radius = Math.cbrt((3 * this.mass) / 4 * Math.PI)
     }
 
     update(otherPlanets) {
@@ -166,13 +182,25 @@ class Planet {
         ctx.beginPath();       // Start a new path
         ctx.strokeStyle = this.color
         ctx.moveTo(this.coords.x, this.coords.y);
-        const dir = this.coords.add(f.scale(.01))// Move the pen to (30, 50)
+        const dir = this.coords.add(f.scale(.0005*this.radius))// Move the pen to (30, 50)
         ctx.lineTo(dir.x, dir.y);  // Draw a line to (150, 100)
         ctx.stroke();
     }
 
     render () {
-        drawCircle(ctx, this.coords.x, this.coords.y, this.radius, this.color)
+        if (this.glow) {
+            ctx.shadowBlur = 20;
+            ctx.shadowColor = this.color;
+        } else {
+            ctx.shadowBlur = 0;
+        }
+        drawCircle(
+            ctx,
+            config.scale * this.coords.x + canvas.width / 2,
+            config.scale * this.coords.y + canvas.height / 2,
+            (config.scale / 2) * this.radius,
+            this.color
+        )
       //  this.drawForce()
        // this.drawVelocity()
 
@@ -182,13 +210,34 @@ class Planet {
 class World {
     constructor(planets=[]) {
         this.planets = planets.map(p=>{
-            return new Planet(p.color, p.mass, p.coords, p.velocity)
+            return new Planet({
+                color: p.color,
+                mass: p.mass,
+                coords: p.coords,
+                velocity: p.velocity,
+                glow: p.glow
+            })
         })
     }
 
     update() {
+        ctx.save()
+        const largestPlanet = this.planets.find(p=>p.color === 'blue')
+
+        const center = {
+            x: canvas.width / 2,
+            y: canvas.height / 2
+        }
+        const translate = {
+            x: center.x - largestPlanet.coords.x,
+            y: center.y - largestPlanet.coords.y
+        }
+        console.log(translate, largestPlanet)
+        config.followCam && ctx.translate(translate.x, translate.y)
+
+
         if (config.renderTrace) {
-            ctx.fillStyle = "rgba(23,24,33,0.2)";
+            ctx.fillStyle = "rgba(23,24,33,0.1)";
             ctx.fillRect(0,0, canvas.width, canvas.height)
         } else {
             ctx.clearRect(0,0, canvas.width, canvas.height)
@@ -197,6 +246,9 @@ class World {
             planet.update(this.planets)
             planet.render()
         })
+
+        ctx.restore()
+
     }
 }
 
