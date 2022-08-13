b1c2d39eb77fe17d372db49f48ed32eb3acdabd9
labtorie <labtorie@gmail.com>
2022-08-14 01:18:27 +0300

    stars + planet cam

--- index.js
+++ index.js
@@ -8,54 +8,107 @@ const TIMEOUT = 1000 / FRAMERATE
 
 const G = 1
 
-document.querySelector('#trace').addEventListener('change', function () {
-
-    config.renderTrace = this.checked
-})
-
-document.querySelector('#forces').addEventListener('change', function () {
-    ctx.clearRect(0,0, canvas.width, canvas.height)
-    config.renderForces = this.checked
-})
-/*document.querySelector('#follow').addEventListener('change', function () {
-    ctx.clearRect(0,0, canvas.width, canvas.height)
-    config.followCam = this.checked
-})*/
-window.addEventListener('scroll', (e)=>{
-    e.preventDefault()
-})
-window.addEventListener('wheel', function (e){
-    const delta = e.deltaY / 500
-    config.scale = Math.max(.01, config.scale - delta)
-})
-
-const pan = {
-    x: 0,
-    y: 0,
-}
-addEventListener('mousemove', e=>{
-    if (!e.buttons) return
-
-    pan.x += e.movementX
-    pan.y += e.movementY
-
-})
-
-
-function force (p1, p2) {
-    //const p2 = new Planet('black', 4000, new Vector2(300, 200), new Vector2(0,0))
-    const f_v = G * (p1.mass * p2.mass) / p1.coords.distance(p2.coords)**2
-    const f_n = p2.coords.subtract(p1.coords).normalize()
-    return f_n.scale(f_v)
-}
-
 config = {
+    play: true,
+    renderStars: true,
     renderTrace: false,
     renderForces: false,
     followCam: false,
     scale: .5
 }
+document.querySelector('#play').addEventListener('change', function () {
+    config.play = this.checked
+})
+document.querySelector('#stars').addEventListener('change', function () {
+    config.renderStars = this.checked
+})
 
+document.querySelector('#trace').addEventListener('change', function () {
+    config.renderTrace = this.checked
+})
+
+document.querySelector('#forces').addEventListener('change', function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    config.renderForces = this.checked
+})
+
+window.addEventListener('scroll', (e) => {
+    e.preventDefault()
+})
+
+const timer = {
+    n: 0
+}
+window.addEventListener('wheel', function (e) {
+    clearTimeout(timer.n)
+    config.renderTrace = false
+    const delta = e.deltaY / 500
+    config.scale = Math.max(.01, config.scale - delta)
+    timer.n = setTimeout(()=>{
+        config.renderTrace = document.querySelector('#trace').checked
+    }, 200)
+})
+
+
+function toCanvas(x, y, depth = 0) {
+    const k = 1
+    const scale = depth === 0 ?
+        config.scale :
+        1 + config.scale / (depth * 10)
+
+    return {
+        x: (pan.x / (1 + depth)) + scale * x + canvas.width / 2,
+        y: (pan.y / (1 + depth)) + scale * y + canvas.height / 2
+    }
+}
+
+const pan = {
+    x: 0,
+    y: 0,
+}
+addEventListener('mousemove', e => {
+    if (!e.buttons) return
+
+    config.renderTrace = false
+    pan.x += e.movementX
+    pan.y += e.movementY
+})
+
+addEventListener('mouseup', ()=>{
+    config.renderTrace = document.querySelector('#trace').checked
+})
+
+addEventListener("touchstart", touchStart, false)
+
+function touchStart(event) {
+    start.x = event.touches[0].pageX;
+    start.y = event.touches[0].pageY;
+}
+
+addEventListener('touchmove', e => {
+    console.log(e)
+    pan.x -= .03 * (start.x - e.touches[0].pageX);
+    pan.y -= .03 * (start.y - e.touches[0].pageY);
+})
+
+
+function force(p1, p2) {
+    //const p2 = new Planet('black', 4000, new Vector2(300, 200), new Vector2(0,0))
+    const f_v = G * (p1.mass * p2.mass) / p1.coords.distance(p2.coords) ** 2
+    const f_n = p2.coords.subtract(p1.coords).normalize()
+    return f_n.scale(f_v)
+}
+
+function generateStars(layers = 4, amount = 500) {
+    return Array(layers).fill(0).map(layer => (
+        Array(amount / layers).fill(0).map(star => ({
+            x: 1000 - (Math.random() * 2000),
+            y: 1000 - (Math.random() * 2000),
+        }))
+    ))
+}
+
+console.log(generateStars())
 
 const planets = [
     {
@@ -68,7 +121,7 @@ const planets = [
         color: 'gray',
         mass: 500,
         coords: new Vector2(120, 0),
-        velocity: new Vector2(0, 3)
+        velocity: new Vector2(0, -3)
     },
     {
         color: 'white',
@@ -80,54 +133,32 @@ const planets = [
         color: '#ffd6d6',
         glow: true,
         mass: 100000,
-        coords: new Vector2(0,0),
+        coords: new Vector2(0, 0),
         velocity: new Vector2(0, -.05)
     },
-
-]
-/*const planets = [
     {
-        color: 'blue',
-        mass: 30000,
-        coords: new Vector2(3 * canvas.width / 8, canvas.height / 2),
-        velocity: new Vector2(0, .5)
-    },
-    {
-        color: 'red',
-        mass: 30000,
-        coords: new Vector2(5 * canvas.width / 8, canvas.height / 2),
-        velocity: new Vector2(0, -.5)
-    },
-]*/
-const colors = [
-    'green', 'blue', 'white', 'purple', 'cyan', 'yellow'
-]
-
-function randomTo(to, floor=true) {
-    if (!floor)
-        return (Math.random()*to)
-    return Math.floor(Math.random()*to)
-}
-
-function generatePlanets (amount) {
-    let c = 0
-    let planets = []
-    while (c<amount) {
-        planets.push({
-            color: colors[randomTo(colors.length-1)],
-            mass: randomTo(120000),
-            coords: new Vector2(randomTo(canvas.width), randomTo(canvas.height)),
-            velocity: new Vector2(randomTo(1, false)- .5, randomTo(1,false)-.5)
-        })
-        c++
+        color: 'orange',
+        mass: 3000,
+        coords: new Vector2(-800, 0),
+        velocity: new Vector2(0, 1.2)
     }
-    return planets
+
+]
+
+function randomTo(to, floor = true) {
+    if (!floor)
+        return (Math.random() * to)
+    return Math.floor(Math.random() * to)
 }
 
-function start () {
-    const world = new World(planets)
+
+function start() {
+    const world = new World(planets, generateStars())
+    document.querySelector('#cam').addEventListener('click', ()=>{
+        world.toggleCamMode()
+    })
     window.world = world
-    const timer = setInterval(()=>{
+    const timer = setInterval(() => {
         world.update()
     }, TIMEOUT)
 
@@ -157,7 +188,7 @@ function drawCircle(ctx, x, y, radius, fill) {
 class Planet {
     constructor({color, mass, coords, velocity, glow}) {
         this.glow = !!glow
-        this.id = Math.floor(Math.random()*10000).toString()
+        this.id = Math.floor(Math.random() * 10000).toString()
         this.color = color
         this.mass = mass
         this.coords = coords
@@ -166,13 +197,13 @@ class Planet {
     }
 
     update(otherPlanets) {
-        let a_sum = new Vector2(0,0)
-        otherPlanets.forEach((planet)=>{
+        let a_sum = new Vector2(0, 0)
+        otherPlanets.forEach((planet) => {
             if (planet.id === this.id)
                 return
 
             const f = force(this, planet)
-            config.renderForces && this.drawForce(f)
+            config.renderForces && this.drawForce(f, planet.color)
             const a_v = f.magnitude() / (this.mass * FRAMERATE)
             let a = f.normalize().scale(a_v)
             a_sum = a_sum.add(a)
@@ -191,39 +222,48 @@ class Planet {
         ctx.stroke();
     }
 
-    drawForce(f) {
-       // const f = force(this)
+    drawForce(f, color) {
+        // const f = force(this)
         ctx.beginPath();       // Start a new path
-        ctx.strokeStyle = this.color
-        ctx.moveTo(this.coords.x, this.coords.y);
-        const dir = this.coords.add(f.scale(.0005*this.radius))// Move the pen to (30, 50)
-        ctx.lineTo(dir.x, dir.y);  // Draw a line to (150, 100)
+        ctx.strokeStyle = color
+        const from = toCanvas(this.coords.x, this.coords.y)
+        ctx.moveTo(from.x, from.y);
+        const dir = this.coords.add(f.scale(.02))// Move the pen to (30, 50)
+        const to = toCanvas(dir.x, dir.y)
+        ctx.lineTo(to.x, to.y);  // Draw a line to (150, 100)
         ctx.stroke();
     }
 
-    render () {
+    render(follow) {
+        if (follow) {
+            pan.x = -this.coords.x
+            pan.y = -this.coords.y
+            config.scale = 1
+        }
         if (this.glow) {
             ctx.shadowBlur = 20;
             ctx.shadowColor = this.color;
         } else {
             ctx.shadowBlur = 0;
         }
+        const {x, y} = toCanvas(this.coords.x, this.coords.y)
+
+        ctx.fillRect(x, y, 1, 1)
         drawCircle(
             ctx,
-            pan.x + config.scale * this.coords.x + canvas.width / 2,
-            pan.y + config.scale * this.coords.y + canvas.height / 2,
+            x, y,
             (config.scale / 2) * this.radius,
             this.color
         )
-      //  this.drawForce()
-       // this.drawVelocity()
 
     }
 }
 
 class World {
-    constructor(planets=[]) {
-        this.planets = planets.map(p=>{
+    constructor(planets = [], stars = []) {
+        this.stars = stars
+        this.camMode = -1
+        this.planets = planets.map(p => {
             return new Planet({
                 color: p.color,
                 mass: p.mass,
@@ -234,9 +274,35 @@ class World {
         })
     }
 
+    renderStars() {
+        this.stars.forEach((stars, depth) => {
+            stars.forEach(star => {
+                const {x, y} = toCanvas(star.x, star.y, depth + 1)
+
+                ctx.fillStyle = '#ffffff'
+                ctx.fillRect(x, y, 1, 1)
+            })
+        })
+    }
+
+    toggleCamMode() {
+        if (this.camMode + 1 === this.planets.length) {
+            document.querySelector('#cam_mode').textContent = 'free'
+            this.camMode = -1
+            pan.x = 0
+            pan.y = 0
+            config.scale = 1
+        }
+        else {
+            document.querySelector('#cam_mode').textContent = 'Planet '+ Number(this.camMode + 1)
+            this.camMode++
+        }
+    }
+
     update() {
         ctx.save()
-        const largestPlanet = this.planets.find(p=>p.color === 'blue')
+
+        const largestPlanet = this.planets.find(p => p.color === 'blue')
 
         const center = {
             x: canvas.width / 2,
@@ -251,13 +317,14 @@ class World {
 
         if (config.renderTrace) {
             ctx.fillStyle = "rgba(23,24,33,0.1)";
-            ctx.fillRect(0,0, canvas.width, canvas.height)
+            ctx.fillRect(0, 0, canvas.width, canvas.height)
         } else {
-            ctx.clearRect(0,0, canvas.width, canvas.height)
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
         }
-        this.planets.forEach(planet=>{
-            planet.update(this.planets)
-            planet.render()
+        config.renderStars && this.renderStars()
+        this.planets.forEach((planet, index) => {
+            config.play && planet.update(this.planets)
+            planet.render(this.camMode === index)
         })
 
         ctx.restore()
@@ -266,7 +333,6 @@ class World {
 }
 
 start()
-
 
 
 //drawCircle(ctx, 300, 300, 20, 'black')
